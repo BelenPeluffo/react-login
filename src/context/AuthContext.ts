02240fb9d133ceb3b0932339976e6ffb0e96e08e
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-29 13:25:11 -0300

    feat (AuthContext.ts): changes AuthState interface and AuthContext's default value.

--- src/context/AuthContext.ts
+++ src/context/AuthContext.ts
@@ -1,11 +1,22 @@
 import { createContext } from "react";
+import { LoginData, User } from "../services/AuthServices";
 
 export interface AuthState {
-  username: string;
-  sign: string;
+  user: User;
+  token: string;
+  login: (userData: LoginData) => void;
+  logout: () => void;
 }
 
+// const defaultData = { email: "", password: "" };
+
 export const AuthContext = createContext<AuthState>({
-  username: "",
-  sign: "",
+  user: {} as User,
+  token: "",
+  login: (defaultData) => {
+    console.log("data?", defaultData);
+  },
+  logout: () => {
+    console.log("void");
+  },
 });

ba81e433a0fc6f5fa36444782ce9663400404af0
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-29 17:11:05 -0300

    feat (Logged.tsx): changes style and structure.

--- src/pages/Logged.tsx
+++ src/pages/Logged.tsx
@@ -1,9 +1,20 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { Box, Button, Typography } from "@mui/material";
+import "../assets/styles.css";
 
 const Logged = () => {
   const auth = useContext(AuthContext);
-  return <div>Bienvenidx, {auth.user.name}</div>;
+  return (
+    <Box className="card-container">
+      <Typography variant="h3" textAlign="center" sx={{ fontWeight: "700" }}>
+        Welcome back, {auth.user.name}! We missed you!
+      </Typography>
+      <Button onClick={auth.logout} variant="contained" size="large">
+        Log out
+      </Button>
+    </Box>
+  );
 };
 
 export default Logged;

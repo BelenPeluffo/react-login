1f6aaf68e06c901879c2b12cb73c48e94301fa1b
Belén Peluffo <belen.peluffo@dirmod.com>
2023-12-29 17:08:00 -0300

    feat (main.tsx): adds router.

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { AuthState } from "./context/AuthState.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <AuthState>
-      <App />
-    </AuthState>
+    <BrowserRouter>
+      <AuthState>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </AuthState>
+    </BrowserRouter>
   </React.StrictMode>
 );

9f0e0b554b97f3065e77e8001cbfa27749cffba1
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-03 12:33:38 -0300

    refactor (register.ts): updates validation schema interface and fields config.

--- src/config/register.ts
+++ src/config/register.ts
@@ -4,42 +4,17 @@ import * as Yup from "yup";
 export const registerFields: RegisterField[] = [
   {
     label: "Name",
-    validation: {
-      required: {
-        value: true,
-        message: `required: name`,
-      },
-      disabled: true,
-    },
   },
   {
     label: "Email address",
-    validation: {
-      required: {
-        value: true,
-        message: `required: email address`,
-      },
-    },
   },
   {
     label: "Password",
     type: "password",
-    validation: {
-      required: {
-        value: true,
-        message: `required: password`,
-      },
-    },
   },
   {
     label: "Confirm password",
     type: "password",
-    validation: {
-      required: {
-        value: true,
-        message: `required: password confirmation`,
-      },
-    },
   },
 ];
 
@@ -47,7 +22,7 @@ export const registerValidationSchema = Yup.object().shape({
   name: Yup.string().required("Name is required"),
   email: Yup.string().email("Email is invalid").required("Email is required"),
   password: Yup.string().required("Password is required"),
-  "confirm password": Yup.string()
+  confirm: Yup.string()
     .required("Confirmation is needed")
     .oneOf([Yup.ref("password"), ""], "Confirm password does not match"),
 });

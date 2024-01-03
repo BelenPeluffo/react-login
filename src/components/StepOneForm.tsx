586ce94813597996d7d543cd892db5baabd49cfc
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-03 14:26:30 -0300

    feat (StepOneForm.tsx): creates form.

--- src/components/StepOneForm.tsx
+++ src/components/StepOneForm.tsx
@@ -1,13 +1,59 @@
-import { Box, Typography } from "@mui/material";
-import { Form, FormProvider, useForm } from "react-hook-form";
+import { Box, Button, Stack, Typography } from "@mui/material";
+import { FormProvider, useForm } from "react-hook-form";
+import FieldInput from "./FieldInput";
+import { StepOneData, stepOneFields } from "../config/stepOneForm";
+import { useEffect, useState } from "react";
 
 const StepOneForm = () => {
-  const methods = useForm();
+  const [stepOneData, setStepOneData] = useState<StepOneData>({
+    documentType: 2,
+    documentNumber: 2,
+    gender: 2,
+    lastname: "string",
+    name: "string",
+    dateOfBirth: new Date(),
+    countryOfBirth: "string",
+    provinceOfBirth: "string",
+    placeOfBirth: "string",
+    schoolYear: 2,
+    grade: 2,
+  });
+  const methods = useForm<StepOneData>();
+
+  useEffect(() => {
+    console.log("stepOneFields?", stepOneFields);
+  }, []);
+
   return (
-    <Box>
+    <Box
+      sx={{
+        width: "100%",
+        border: "solid 1px",
+        borderRadius: "5px",
+        my: 1,
+      }}
+    >
       <Typography variant="h5">Información dxl estudiante</Typography>
       <FormProvider {...methods}>
-        <Form></Form>
+        <form
+          noValidate
+          onSubmit={(event) => event.preventDefault}
+          style={{ display: "flex", flexDirection: "row" }}
+        >
+          <Stack gap={2}>
+            {stepOneFields.map((field) => (
+              <FieldInput<StepOneData>
+                label={field.label}
+                referenceObject={stepOneData}
+                setObject={setStepOneData}
+                key={field.label}
+                name={field.id}
+                type={field.type}
+              />
+            ))}
+          </Stack>
+          <Button>Yas</Button>
+        </form>
       </FormProvider>
     </Box>
   );

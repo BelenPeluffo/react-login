7586a962079c69b602bbfa2947abdabc49502ae7
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-03 14:15:01 -0300

    feat (Logged.tsx): adds form.

--- src/pages/Logged.tsx
+++ src/pages/Logged.tsx
@@ -2,14 +2,16 @@ import { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
 import { Box, Button, Typography } from "@mui/material";
 import "../assets/styles.css";
+import StepOneForm from "../components/StepOneForm";
 
 const Logged = () => {
   const auth = useContext(AuthContext);
   return (
-    <Box className="card-container">
-      <Typography variant="h3" textAlign="center" sx={{ fontWeight: "700" }}>
+    <Box className="big-container">
+      <Typography variant="h5" textAlign="center" sx={{ fontWeight: "700" }}>
         Welcome back, {auth.user.name}! We missed you!
       </Typography>
+      <StepOneForm />
       <Button onClick={auth.logout} variant="contained" size="large">
         Log out
       </Button>

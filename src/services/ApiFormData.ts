ccd7bcc3e715e55f1958878f395517ced773c4cb
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-03 14:38:08 -0300

    feat (ApiFormData.ts): creates API mocked data.

---
+++ src/services/ApiFormData.ts
@@ -0,0 +1,27 @@
+import { Option } from "../config/stepOneForm";
+
+export const documentTypes: Option[] = [
+  {
+    value: 1,
+    label: "DNI",
+  },
+  {
+    value: 2,
+    label: "Passaport",
+  },
+];
+
+export const genders: Option[] = [
+    {
+        value: 1,
+        label: "Female",
+      },
+      {
+        value: 2,
+        label: "Male",
+      },
+      {
+        value: 3,
+        label: "Non binary",
+      },
+]

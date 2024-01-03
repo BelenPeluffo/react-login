5b33a019c41fe3848a283052d514ca63877feea5
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-03 14:12:43 -0300

    feat (stepOneForm.ts): creates Option and StepOneData interfaces; adds 'id' property to objects in stepOneFields.

--- src/config/stepOneForm.ts
+++ src/config/stepOneForm.ts
@@ -1,48 +1,78 @@
 import { FormField } from "./register";
 
+export interface Option {
+  value: number;
+  label: string;
+}
+
+export interface StepOneData {
+  documentType: number;
+  documentNumber: number;
+  gender: number;
+  lastname: string;
+  name: string;
+  dateOfBirth: Date;
+  countryOfBirth: string;
+  provinceOfBirth: string;
+  placeOfBirth: string;
+  schoolYear: number;
+  grade: number;
+}
+
 export const stepOneFields: FormField[] = [
-    {
-        label: "Tipo de documento",
-        type: "dropdown",
-    },
-    {
-        label: "Número de documento",
-        type: "text",
-    },
-    {
-        label: "Género",
-        type: "dropdown",
-    },
-    {
-        label: "Apellido/s",
-        type: "text",
-    },
-    {
-        label: "Nombre/s",
-        type: "text",
-    },
-    {
-        label: "Fecha de nacimiento",
-        type: "date",
-    },
-    {
-        label: "País de nacimiento",
-        type: "text",
-    },
-    {
-        label: "Provincia de nacimiento",
-        type: "text",
-    },
-    {
-        label: "Localidad de nacimiento",
-        type: "text",
-    },
-    {
-        label: "Ciclo lectivo",
-        type: "text",
-    },
-    {
-        label: "Año/Ciclo",
-        type: "text",
-    },
+  {
+    label: "Tipo de documento",
+    type: "dropdown",
+    id: "documentType",
+  },
+  {
+    label: "Número de documento",
+    type: "text",
+    id: "documentNumber",
+  },
+  {
+    label: "Género",
+    type: "dropdown",
+    id: "gender",
+  },
+  {
+    label: "Apellido/s",
+    type: "text",
+    id: "lastname",
+  },
+  {
+    label: "Nombre/s",
+    type: "text",
+    id: "name",
+  },
+  {
+    label: "Fecha de nacimiento",
+    type: "date",
+    id: "dateOfBirth",
+  },
+  {
+    label: "País de nacimiento",
+    type: "text",
+    id: "countryOfBirth",
+  },
+  {
+    label: "Provincia de nacimiento",
+    type: "text",
+    id: "provinceOfBirth",
+  },
+  {
+    label: "Localidad de nacimiento",
+    type: "text",
+    id: "placeOfBirth",
+  },
+  {
+    label: "Ciclo lectivo",
+    type: "text",
+    id: "schoolYear",
+  },
+  {
+    label: "Año/Ciclo",
+    type: "text",
+    id: "grade",
+  },
 ];

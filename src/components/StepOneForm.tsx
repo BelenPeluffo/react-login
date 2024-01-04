ef91327b28bc1148c12b43d56e92bb4aa9de4213
Belén Peluffo <belen.peluffo@dirmod.com>
2024-01-04 17:19:22 -0300

    feat (StepOneForm.tsx): creates disabilitySetter() and handles disability state for form fields.

--- src/components/StepOneForm.tsx
+++ src/components/StepOneForm.tsx
@@ -11,6 +11,7 @@ import { FormProvider, useForm } from "react-hook-form";
 import FieldInput from "./FieldInput";
 import {
   StepOneData,
+  disabilityParser,
   stepOneFields,
   stepOneFormValidationSchema,
 } from "../config/stepOneForm";
@@ -18,20 +19,26 @@ import { useEffect, useState } from "react";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useCases } from "../services/ApiFormData";
 
+const initialValue: StepOneData = {
+  documentType: 2,
+  documentNumber: 2,
+  gender: 2,
+  lastname: "",
+  name: "",
+  dateOfBirth: new Date(),
+  countryOfBirth: "",
+  provinceOfBirth: "",
+  placeOfBirth: "",
+  schoolYear: 2,
+  grade: 2,
+};
+
 const StepOneForm = () => {
-  const [stepOneData, setStepOneData] = useState<StepOneData>({
-    documentType: 2,
-    documentNumber: 2,
-    gender: 2,
-    lastname: "",
-    name: "",
-    dateOfBirth: new Date(),
-    countryOfBirth: "",
-    provinceOfBirth: "",
-    placeOfBirth: "",
-    schoolYear: 2,
-    grade: 2,
-  });
+  const [stepOneData, setStepOneData] = useState<StepOneData>(initialValue);
+  const [useCase, setUseCase] = useState(0);
+  const [disabledFields, setDisabledFields] = useState<{
+    [index: keyof StepOneData]: boolean;
+  }>({});
   const methods = useForm<StepOneData>({
     resolver: yupResolver(stepOneFormValidationSchema),
   });
@@ -39,18 +46,34 @@ const StepOneForm = () => {
   const onCancel = () => {};
 
   const onSave = methods.handleSubmit((data) => {
+    // Ésto sólo se va a ejecutar cuando se hayan cumplido todos los requisitos
     console.log("data?", data);
-    // const isValid = stepOneFormValidationSchema.validate(data);
-    // console.log('isValid?',isValid);
   });
 
   const onNext = () => {
     onSave();
   };
 
+  const disabilitySetter = (fields: string[]) => {
+    console.log("fields?", fields);
+    const disabledStates: { [key in keyof StepOneData]: boolean } = {} as {
+      [key in keyof StepOneData]: boolean;
+    };
+    fields.forEach((field) => (disabledStates[field] = true));
+    setDisabledFields(disabledStates);
+  };
+
   useEffect(() => {
-    console.log("stepOneData?", stepOneData);
-  }, [stepOneData]);
+    if (useCase > 0) {
+      console.log("Use case elegido?", useCase);
+      console.log(
+        "Campos a deshabilitar?",
+        disabilityParser(initialValue, useCase)
+      );
+      const disabledFields = disabilityParser(initialValue, useCase);
+      disabilitySetter(disabledFields as string[]);
+    }
+  }, [useCase]);
 
   return (
     <Box
@@ -62,9 +85,16 @@ const StepOneForm = () => {
       }}
     >
       <Typography variant="h5">Información dxl estudiante</Typography>
-      <Box>
+      <Box sx={{ mx: 3 }}>
         <InputLabel>Caso de uso</InputLabel>
-        <Select>
+        <Select
+          fullWidth
+          displayEmpty
+          onChange={(event) => setUseCase(event.target.value as number)}
+        >
+          <MenuItem value="">
+            <em>Seleccioná un caso de uso</em>
+          </MenuItem>
           {useCases.map((useCase) => (
             <MenuItem value={useCase.value}>{useCase.label}</MenuItem>
           ))}
@@ -90,6 +120,7 @@ const StepOneForm = () => {
                 type={field.type}
                 options={field.options}
                 width={field.width}
+                disabled={disabledFields[field.id]}
               />
             ))}
           </Stack>
